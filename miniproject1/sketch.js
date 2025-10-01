dde560d2dc04d0746c3fdd0609d3e64974be36c8
Fiona <firving-beck@ucsb.edu>
2025-09-30 20:26:35 -0700

    adding project 1

---
+++ miniproject1/sketch.js
@@ -0,0 +1,15 @@
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background("pink");
+
+  ellipse(200, 200, 300, 250);
+
+  circle(130, 200, 20, 20);
+  circle(270, 200, 20, 20);
+  triangle(200, 230, 190, 210, 210,210 );
+  line(200, 230, 250, 270);
+  line(200, 230, 150, 270);
+}
